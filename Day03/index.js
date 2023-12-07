da5de7d8cc72822bfb6b296cb98dbce40815e8f5
Pedro Lima e Silva <pedrocaslsilva403@gmail.com>
2023-12-07 20:12:04 +0000

    Day 3, part 1

---
+++ Day03/index.js
@@ -0,0 +1,66 @@
+function isSymbol(char) {
+  return char && isNaN(char) && char !== '.';
+}
+
+function processInput(text) {
+  const lines = text.split('\n');
+  const serials = [];
+  let serialsAcc = 0;
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    console.log(line);
+
+    let possibleSerial = '';
+    let serialIsValid = false;
+    for (let k = 0; k < line.length; k++) {
+      const char = line.charAt(k);
+      if (!isNaN(char)) {
+        // found a number, look for symbol around it
+        possibleSerial += '' + char;
+
+        if (!serialIsValid) {
+          // check horizontal
+          serialIsValid =
+            isSymbol(line.charAt(k - 1)) ||
+            isSymbol(line.charAt(k + 1)) ||
+            // check line above
+            (i > 0 &&
+              (isSymbol(lines[i - 1].charAt(k - 1)) ||
+                isSymbol(lines[i - 1].charAt(k)) ||
+                isSymbol(lines[i - 1].charAt(k + 1)))) ||
+            (i < lines.length - 1 &&
+              (isSymbol(lines[i + 1].charAt(k - 1)) ||
+                isSymbol(lines[i + 1].charAt(k)) ||
+                isSymbol(lines[i + 1].charAt(k + 1))));
+        }
+      } else {
+        // current char is not a number, store the serial if valid and reset the building serial
+        if (serialIsValid) {
+          serialIsValid = false;
+          serialsAcc += parseInt(possibleSerial);
+          console.log(possibleSerial);
+        }
+        possibleSerial = '';
+      }
+    }
+    if (serialIsValid) {
+      serialIsValid = false;
+      serialsAcc += parseInt(possibleSerial);
+      console.log(possibleSerial);
+    }
+    possibleSerial = '';
+  }
+
+  return serialsAcc;
+}
+
+function main() {
+  fetch('input.md')
+    .then((data) => data.text())
+    .then((text) => {
+      document.getElementById('input').textContent = text;
+      document.getElementById('result').innerHTML = processInput(text);
+    });
+}
+
+main();
