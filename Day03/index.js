0e164b5012b314c9b256f1292fb5b4c12a9abc8b
Pedro Lima e Silva <pedrocaslsilva403@gmail.com>
2024-12-05 20:49:46 +0000

    WIP

--- Day03/index.js
+++ Day03/index.js
@@ -1,50 +1,50 @@
-function isSymbol(char) {
-  return char && isNaN(char) && char !== '.';
+function isNextToStar(char) {
+  return char && isNaN(char) && char === '*';
 }
 
 function processInput(text) {
   const lines = text.split('\n');
-  const serials = [];
   let serialsAcc = 0;
+
   for (let i = 0; i < lines.length; i++) {
     const line = lines[i];
     console.log(line);
 
     let possibleSerial = '';
-    let serialIsValid = false;
+    let isPossibleCog = false;
     for (let k = 0; k < line.length; k++) {
       const char = line.charAt(k);
       if (!isNaN(char)) {
         // found a number, look for symbol around it
         possibleSerial += '' + char;
 
-        if (!serialIsValid) {
+        if (!isPossibleCog) {
           // check horizontal
-          serialIsValid =
-            isSymbol(line.charAt(k - 1)) ||
-            isSymbol(line.charAt(k + 1)) ||
+          isPossibleCog =
+            isNextToStar(line.charAt(k - 1)) ||
+            isNextToStar(line.charAt(k + 1)) ||
             // check line above
             (i > 0 &&
-              (isSymbol(lines[i - 1].charAt(k - 1)) ||
-                isSymbol(lines[i - 1].charAt(k)) ||
-                isSymbol(lines[i - 1].charAt(k + 1)))) ||
+              (isNextToStar(lines[i - 1].charAt(k - 1)) ||
+                isNextToStar(lines[i - 1].charAt(k)) ||
+                isNextToStar(lines[i - 1].charAt(k + 1)))) ||
             (i < lines.length - 1 &&
-              (isSymbol(lines[i + 1].charAt(k - 1)) ||
-                isSymbol(lines[i + 1].charAt(k)) ||
-                isSymbol(lines[i + 1].charAt(k + 1))));
+              (isNextToStar(lines[i + 1].charAt(k - 1)) ||
+                isNextToStar(lines[i + 1].charAt(k)) ||
+                isNextToStar(lines[i + 1].charAt(k + 1))));
         }
       } else {
         // current char is not a number, store the serial if valid and reset the building serial
-        if (serialIsValid) {
-          serialIsValid = false;
+        if (isPossibleCog) {
+          isPossibleCog = false;
           serialsAcc += parseInt(possibleSerial);
           console.log(possibleSerial);
         }
         possibleSerial = '';
       }
     }
-    if (serialIsValid) {
-      serialIsValid = false;
+    if (isPossibleCog) {
+      isPossibleCog = false;
       serialsAcc += parseInt(possibleSerial);
       console.log(possibleSerial);
     }
