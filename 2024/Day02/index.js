d0e29cf504f61db805fe36a2d9a172567cb8087a
Pedro Lima e Silva <pedro.silva2@sky.uk>
2024-12-06 09:36:26 +0000

    Day 2 Part 2 finished

--- 2024/Day02/index.js
+++ 2024/Day02/index.js
@@ -1,8 +1,8 @@
-const input = document.getElementById('input');
-const submitButton = document.getElementById('submit');
-const result = document.getElementById('result');
+const input = document.getElementById("input");
+const submitButton = document.getElementById("submit");
+const result = document.getElementById("result");
 
-submitButton.addEventListener('click', rateReports);
+submitButton.addEventListener("click", rateReports);
 
 function isReportSafe(report) {
   let prevEntry;
@@ -10,7 +10,6 @@ function isReportSafe(report) {
   console.groupEnd();
 
   console.group(report);
-  let hasSkipped = false;
   for (let i = 0; i < report.length; i++) {
     const entry = +report[i];
     if (prevEntry === undefined) {
@@ -18,75 +17,69 @@ function isReportSafe(report) {
       continue;
     }
     if (prevEntry === entry) {
-      console.log('repeated entries', { prevEntry, entry });
-      if (hasSkipped) {
-        return false;
-      } else {
-        console.log('dampening', { i, entry });
-        hasSkipped = true;
-        prevEntry = entry;
-        continue;
-      }
+      console.log("repeated entries", { prevEntry, entry });
+      return { safe: false, index: i };
     }
     if (prevEntry !== entry) {
       if (isAscending === undefined) {
         isAscending = prevEntry < entry;
-        console.log('isAscending', isAscending, { prevEntry, entry });
+        // console.log("isAscending", isAscending, { prevEntry, entry });
       } else {
         if (isAscending && prevEntry > entry) {
-          console.log('stopped ascending', { prevEntry, entry });
-          if (hasSkipped) {
-            return false;
-          } else {
-            console.log('dampening', { i, entry });
-            hasSkipped = true;
-            prevEntry = entry;
-            continue;
-          }
+          console.log("stopped ascending", { prevEntry, entry });
+          return { safe: false, index: i };
         } else if (!isAscending && prevEntry < entry) {
-          console.log('stopped descending', { prevEntry, entry });
-          if (hasSkipped) {
-            return false;
-          } else {
-            console.log('dampening', { i, entry });
-            hasSkipped = true;
-            prevEntry = entry;
-            continue;
-          }
+          console.log("stopped descending", { prevEntry, entry });
+          return { safe: false, index: i };
         }
       }
       const distance = Math.abs(prevEntry - entry);
       if (distance > 3) {
-        console.log('distance bigger than 3', distance, { prevEntry, entry });
-        if (hasSkipped) {
-          return false;
-        } else {
-          console.log('dampening', { i, entry });
-          hasSkipped = true;
-          prevEntry = entry;
-          continue;
-        }
+        console.log("distance bigger than 3", distance, { prevEntry, entry });
+        return { safe: false, index: i };
       }
     }
     prevEntry = entry;
   }
-  console.info('is Safe')
-  return true;
+  console.info("is Safe");
+  return { safe: true };
+}
+
+function getDampenedOptions(report) {
+  let options = [];
+  for (let i = 0; i < report.length; i++) {
+    let dampenedEntryOption = [...report];
+    dampenedEntryOption.splice(i, 1);
+    options.push(dampenedEntryOption);
+  }
+  return options;
 }
 
 function rateReports() {
-  result.textContent = 'Calculating...';
+  result.textContent = "Calculating...";
 
   const text = input.value;
-  const lines = text.split('\n');
+  const lines = text.split("\n");
   let sum = 0;
   for (let l = 0; l < lines.length; l++) {
-    const entries = lines[l].split(' ');
-    if (isReportSafe(entries)) {
+    const entries = lines[l].split(" ");
+    const result = isReportSafe(entries);
+    if (result.safe) {
       sum++;
+      continue;
+    } else if (result.index) {
+      const options = getDampenedOptions(entries);
+      for (let i = 0; i < options.length; i++) {
+        const option = options[i];
+        if (isReportSafe(option).safe) {
+          sum++;
+          console.log("found option that works", option);
+          break;
+        }
+      }
     }
-    result.textContent = sum;
   }
+  result.textContent = sum;
   console.log(sum);
   return sum;
 }
