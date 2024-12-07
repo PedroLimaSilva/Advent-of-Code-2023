a849981e2910b839f76ad194f9ad8b589449cd96
Pedro Lima e Silva <pedrocaslsilva403@gmail.com>
2024-12-07 02:28:18 +0000

    Day 4 part 2; Who needs Generalizations?

--- 2024/Day04/index.js
+++ 2024/Day04/index.js
@@ -22,12 +22,7 @@ function checkDirection(matrix, line, column, direction, targetCharIndex) {
     l: line + DIRECTIONS[direction][0],
     c: column + DIRECTIONS[direction][1],
   };
-  if (
-    newPosition.c < 0 ||
-    newPosition.l < 0 ||
-    newPosition.l >= matrix.length ||
-    newPosition.c > matrix[newPosition.l].length
-  ) {
+  if (checkOutOfBounds(matrix, newPosition)) {
     // Out of bounds check
     return false;
   }
@@ -47,7 +42,7 @@ function checkDirection(matrix, line, column, direction, targetCharIndex) {
   }
 }
 
-function getAnswer() {
+function part1() {
   result.textContent = 'Calculating...';
 
   const text = input.value;
@@ -64,6 +59,85 @@ function getAnswer() {
             count++;
           }
         });
+      }
+    }
+  }
+
+  console.log(matrix);
+
+  result.textContent = count;
+  console.log(count);
+  return count;
+}
+
+function checkOutOfBounds(matrix, position) {
+  if (
+    position.c < 0 ||
+    position.l < 0 ||
+    position.l >= matrix.length ||
+    position.c > matrix[position.l].length
+  ) {
+    // Out of bounds check
+    return true;
+  }
+}
+
+const X_MAS_CHAR = {
+  M: true,
+  S: true,
+};
+
+function getAnswer() {
+  result.textContent = 'Calculating...';
+
+  const text = input.value;
+  const matrix = text.split('\n');
+  let count = 0;
+
+  for (let l = 0; l < matrix.length; l++) {
+    for (let c = 0; c < matrix[l].length; c++) {
+      const char = matrix[l][c];
+      if (char === 'A') {
+        console.log(`found ${char} at [${l}, ${c}]`);
+
+        const nw = { l: l + DIRECTIONS.NW[0], c: c + DIRECTIONS.NW[1] };
+        const ne = { l: l + DIRECTIONS.NE[0], c: c + DIRECTIONS.NE[1] };
+        const sw = { l: l + DIRECTIONS.SW[0], c: c + DIRECTIONS.SW[1] };
+        const se = { l: l + DIRECTIONS.SE[0], c: c + DIRECTIONS.SE[1] };
+        if (
+          checkOutOfBounds(matrix, nw) ||
+          checkOutOfBounds(matrix, ne) ||
+          checkOutOfBounds(matrix, sw) ||
+          checkOutOfBounds(matrix, se)
+        ) {
+          continue;
+        }
+        let masCount = 0;
+        if (matrix[nw.l][nw.c] === 'M') {
+          if (matrix[se.l][se.c] === 'S') {
+            masCount++;
+          }
+        }
+        if (matrix[nw.l][nw.c] === 'S') {
+          if (matrix[se.l][se.c] === 'M') {
+            masCount++;
+          }
+        }
+        if (masCount === 0) {
+          continue;
+        }
+        if (matrix[ne.l][ne.c] === 'M') {
+          if (matrix[sw.l][sw.c] === 'S') {
+            count++;
+            continue;
+          }
+        }
+        if (matrix[ne.l][ne.c] === 'S') {
+          if (matrix[sw.l][sw.c] === 'M') {
+            count++;
+            continue;
+          }
+        }
       }
     }
   }
